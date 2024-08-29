e8c078196f088bd68c21168ff2bcaceb3cee8276
Stephen Appel <srappel930@gmail.com>
2024-08-29 12:02:08 -0500

    update task schedules

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@
 # Learn more: http://github.com/javan/whenever
 
 # Harvest thumbnail images for search results
-every :day, at: "12:05am", roles: [:app] do
+every :sunday, at: "12:05am", roles: [:app] do
   rake "gblsci:images:harvest_retry"
 end
 
@@ -14,27 +14,27 @@ every :day, at: "12:30am", roles: [:app] do
 end
 
 # Cleans up anonymous user accounts created by search sessions
-every :day, at: "1:30am", roles: [:app] do
+every :tuesday, at: "1:30am", roles: [:app] do
   rake "devise_guests:delete_old_guest_users[2]"
 end
 
 # Cleans up recent anonymous search records
-every :day, at: "2:00am", roles: [:app] do
+every :tuesday, at: "2:00am", roles: [:app] do
   rake "blacklight:delete_old_searches[7]"
 end
 
 # Runs the OpenDataHarvest DCAT script
-every :day, at: "2:30am", roles: [:app] do
+every :wednesday, at: "2:30am", roles: [:app] do
   rake "uwm:opendataharvest:harvest_dcat"
 end
 
 # Updates the UWM OpenGeoMetadata directory (git pull) and re-index
-every :day, at: "3:00am", roles: [:app] do
+every :wednesday, at: "3:00am", roles: [:app] do
   command ". /var/www/rubyapps/uwm-geoblacklight/current/bin/geocombine_pull_and_index.sh"
 end
 
 # Harvest Allmaps IIIF Annotation Data
-every :day, at: "3:30am", roles: [:app] do
+every :sunday, at: "2:30am", roles: [:app] do
   rake "blacklight_allmaps:sidecars:harvest:allmaps"
   rake "blacklight_allmaps:index:georeferenced_facet"
 end
