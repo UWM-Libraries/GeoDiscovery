c9a07da49da4cf504bcc2486288c10e6e25a8b22
Stephen Appel <srappel930@gmail.com>
2024-12-09 15:08:40 -0600

    helper method to truncate AND render html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def render_html_value(args)
     simple_format(Array(args[:value]).flatten.join(" "))
   end
+
+  def truncate_render_html_value(args)
+    tag.div class: "truncate-abstract" do
+      simple_format(Array(args[:value]).flatten.join(" "))
+    end
+  end
 end
