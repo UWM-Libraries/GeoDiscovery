6ad75a77a6d348ef0d33de24fc753c343236b96a
Stephen Appel <srappel930@gmail.com>
2025-05-07 16:10:25 -0500

    customize throttle response to 503

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -38,9 +38,11 @@ class Rack::Attack
   # If you want to return 503 so that the attacker might be fooled into
   # believing that they've successfully broken your app (or you just want to
   # customize the response), then uncomment these lines.
-  # self.throttled_response = lambda do |env|
-  #  [ 503,  # status
-  #    {},   # headers
-  #    ['']] # body
-  # end
+  self.throttled_response = lambda do |env|
+    [
+      503,
+      { 'Content-Type' => 'text/plain' },
+      ["Service temporarily unavailable. Please try again later.\n"]
+    ]
+  end
 end
