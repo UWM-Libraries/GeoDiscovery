e02bb73c44a7ec2f56f2565519383b80213e1ff8
Stephen Appel <srappel930@gmail.com>
2025-05-09 10:01:19 -0500

    linter

--- test/middleware/rack_attack_test.rb
+++ test/middleware/rack_attack_test.rb
@@ -14,7 +14,7 @@ class RackAttackTest < ActionDispatch::IntegrationTest
     Rack::Attack.throttled_responder = lambda do |_request|
       [
         503,
-        { "Content-Type" => "text/plain" },
+        {"Content-Type" => "text/plain"},
         ["Service temporarily unavailable. Please try again later.\n"]
       ]
     end
@@ -27,12 +27,12 @@ class RackAttackTest < ActionDispatch::IntegrationTest
 
   test "blocks request after exceeding limit" do
     3.times do
-      get "/robots.txt", headers: { "REMOTE_ADDR" => "1.2.3.4" }
+      get "/robots.txt", headers: {"REMOTE_ADDR" => "1.2.3.4"}
       assert_response :success
     end
 
-    get "/robots.txt", headers: { "REMOTE_ADDR" => "1.2.3.4" }
+    get "/robots.txt", headers: {"REMOTE_ADDR" => "1.2.3.4"}
     assert_response :service_unavailable
-    assert_match /Service temporarily unavailable/, response.body
+    assert_match(/Service temporarily unavailable/, response.body)
   end
 end
