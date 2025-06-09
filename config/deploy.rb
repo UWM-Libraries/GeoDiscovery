2bd65f721792b61dc9bb578572e72e054abdcc93
Stephen Appel <srappel930@gmail.com>
2025-06-09 14:43:12 -0500

    use linked files for .env.production

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :migration_role, :app
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/blacklight.yml", "config/database.yml", "config/master.key"
+append :linked_files, "config/blacklight.yml", "config/database.yml", "config/master.key", ".env.production",
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor",
@@ -63,15 +63,6 @@ namespace :deploy do
 end
 
 namespace :deploy do
-  # Define custom task to copy .example.env.production to .env.production
-  task :copy_env_file do
-    on roles(:app) do
-      within release_path do
-        execute :cp, ".example.env.production", ".env.production"
-      end
-    end
-  end
-
   # Hook the custom task into the deploy process
   after :updated, :copy_env_file
 
