a3e9db5620e998a89dc3e9c6fb486c37e5a46582
Stephen Appel <srappel930@gmail.com>
2025-06-09 14:44:40 -0500

    linter

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :migration_role, :app
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/blacklight.yml", "config/database.yml", "config/master.key", ".env.production",
+append :linked_files, "config/blacklight.yml", "config/database.yml", "config/master.key", ".env.production"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor",
