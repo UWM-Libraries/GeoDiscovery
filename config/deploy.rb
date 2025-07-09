a09660b4f087e73ff412207f24aff8a95a730f2a
Stephen Appel <srappel930@gmail.com>
2025-07-09 11:52:54 -0500

    Passenger configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,5 +80,11 @@ end
 set :whenever_roles, [:app]
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 
+# Passenger configuration
+set :passenger_restart_with_touch, false
+set :passenger_restart_command, -> {
+  "passenger-config restart-app #{fetch(:deploy_to)} --ignore-app-not-running"
+}
+
 before "deploy:updated", "whenever:clear_crontab"
 after "deploy:updated", "whenever:update_crontab"
