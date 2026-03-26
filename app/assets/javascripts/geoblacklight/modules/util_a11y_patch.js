3e325d17695bf54b605038e7d188c46b00d92d6b
Stephen Appel <srappel930@gmail.com>
2026-03-26 14:41:23 -0500

    Add a11y labeling for OpenIndexMaps popup links

---
+++ app/assets/javascripts/geoblacklight/modules/util_a11y_patch.js
@@ -0,0 +1,39 @@
+/*global GeoBlacklight */
+
+(function() {
+  if (!GeoBlacklight || !GeoBlacklight.Util || !GeoBlacklight.Util.indexMapTemplate) {
+    return;
+  }
+
+  function previewLinkLabel(data) {
+    if (data && data.title) return "Open website for " + data.title;
+    if (data && data.label) return "Open website for " + data.label;
+    return "Open website";
+  }
+
+  function addIndexMapImageAccessibility(html, data) {
+    var container = document.createElement("div");
+    container.innerHTML = html;
+
+    Array.prototype.forEach.call(container.querySelectorAll(".index-map-info img"), function(image) {
+      image.setAttribute("alt", "");
+
+      var link = image.closest("a");
+      if (!link) return;
+
+      if (!link.getAttribute("aria-label")) {
+        link.setAttribute("aria-label", previewLinkLabel(data));
+      }
+    });
+
+    return container.innerHTML;
+  }
+
+  var originalIndexMapTemplate = GeoBlacklight.Util.indexMapTemplate;
+
+  GeoBlacklight.Util.indexMapTemplate = function(data, cb) {
+    return originalIndexMapTemplate.call(this, data, function(html) {
+      cb(addIndexMapImageAccessibility(html, data));
+    });
+  };
+}());
--- test/helpers/openindexmaps_template_test.rb
+++ test/helpers/openindexmaps_template_test.rb
@@ -4,6 +4,7 @@ require "test_helper"
 
 class OpenindexmapsTemplateTest < ActiveSupport::TestCase
   TEMPLATE_PATH = Rails.root.join("app/assets/javascripts/geoblacklight/templates/index_map_info.hbs").freeze
+  PATCH_PATH = Rails.root.join("app/assets/javascripts/geoblacklight/modules/util_a11y_patch.js").freeze
 
   test "index map preview images have empty alt text" do
     template = TEMPLATE_PATH.read
@@ -14,6 +15,13 @@ class OpenindexmapsTemplateTest < ActiveSupport::TestCase
   test "linked index map preview images label the link for assistive technology" do
     template = TEMPLATE_PATH.read
 
-    assert_includes template, 'aria-label="{{#if title}}Open preview image for {{title}}'
+    assert_includes template, 'aria-label="{{#if title}}Open website for {{title}}'
+  end
+
+  test "index map popup html is hardened with runtime accessibility attributes" do
+    patch = PATCH_PATH.read
+
+    assert_includes patch, 'image.setAttribute("alt", "")'
+    assert_includes patch, 'link.setAttribute("aria-label", previewLinkLabel(data))'
   end
 end
